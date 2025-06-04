a2a28d6bd98cdc83b0d11c4a23e4d7740b6175d0
Doruto721 <umapiman@gmail.com>
2025-06-04 14:41:31 +0900

    Add files via upload

--- assessment.js
+++ assessment.js
@@ -15,14 +15,27 @@ assessmentButton.addEventListener(
 
     // 診断結果表示エリアの作成
     resultDivision.innerText =''
-    const header = document.createElement('h3');
-    header.innerText = '診断結果';
-    resultDivision.appendChild(header);
+      // headerDivision の作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
+
+    // bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
 
     const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
     const result = assessment(userName);
     paragraph.innerText = result;
-    resultDivision.appendChild(paragraph);
+    bodyDivision.appendChild(paragraph);
+
+    // resultDivision に Bootstrap のスタイルを適用する
+    resultDivision.setAttribute('class', 'card');
+
+    // headerDivision と bodyDivision を resultDivision に差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
 
     // ツイートエリアの作成
     tweetDivision.innerText = '';
@@ -57,22 +70,23 @@ userNameInput.addEventListener(
 
 
 const answers = [
-  '###userName###のいいところは声です。###userName###の特徴的な声は皆を惹きつけ、心に残ります。',
-  '###userName###のいいところはまなざしです。###userName###に見つめられた人は、気になって仕方がないでしょう。',
-  '###userName###のいいところは情熱です。###userName###の情熱に周りの人は感化されます。',
-  '###userName###のいいところは厳しさです。###userName###の厳しさがものごとをいつも成功に導きます。',
-  '###userName###のいいところは知識です。博識な###userName###を多くの人が頼りにしています。',
+  '###userName###のいいところは声です。###userName###の特徴的な声は、友達を減らすのにとても有効です。',
+  '###userName###のいいところは頭です。###username###の頭はぶっ飛んでいて、もはや神の領域です。',
+  '###userName###のいいところはまなざしです。###userName###に見つめられた人は、ぞわぞわして気になって仕方がないでしょう。',
+  '###userName###のいいところは情熱です。###userName###の情熱に周りの人は感化され、いつも逃げます。',
+  '###userName###のいいところは厳しさです。###userName###の厳しさがものごとをいつも成功に導くとは限らないだろ。',
+  '###userName###のいいところは知識です。博識な###userName###を多くの人が頼りにしていると思わないでください。',
   '###userName###のいいところはユニークさです。###userName###だけのその特徴が皆を楽しくさせます。',
-  '###userName###のいいところは用心深さです。###userName###の洞察に、多くの人が助けられます。',
-  '###userName###のいいところは見た目です。内側から溢れ出る###userName###の良さに皆が気を惹かれます。',
-  '###userName###のいいところは決断力です。###userName###がする決断にいつも助けられる人がいます。',
-  '###userName###のいいところは思いやりです。###userName###に気をかけてもらった多くの人が感謝しています。',
+  '###userName###のいいところは用心深さです。###userName###の洞察に、多くの人が助けられ、そしてその後絶望させられます。信じる奴がバカです。',
+  '###userName###のいいところは見た目です。内側から溢れ出る###userName###の良さに皆が気を失います。',
+  '###userName###のいいところは決断力です。###userName###がする決断にいつも助けられ、同時に不幸になる人がいます。',
+  '###userName###のいいところは思いやりです。###userName###に気をかけてもらった多くの人が感謝してい流と思わないでください。勘違いです。',
   '###userName###のいいところは感受性です。###userName###が感じたことに皆が共感し、わかりあうことができます。',
-  '###userName###のいいところは節度です。強引すぎない###userName###の考えに皆が感謝しています。',
-  '###userName###のいいところは好奇心です。新しいことに向かっていく###userName###の心構えが多くの人に魅力的に映ります。',
-  '###userName###のいいところは気配りです。###userName###の配慮が多くの人を救っています。',
-  '###userName###のいいところはそのすべてです。ありのままの###userName###自身がいいところなのです。',
-  '###userName###のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる###userName###が皆から評価されています。'
+  '###userName###のいいところは節度です。強引すぎない###userName###の考えに皆が感謝していますが、同時にみんなの印象にもあまり残っていません。',
+  '###userName###のいいところは好奇心です。新しいことに向かっていく###userName###の心構えが多くの人に踏み出す勇気を与えます。',
+  '###userName###のいいところは気配りです。###userName###の配慮が多くの人を逆に困らせています。',
+  '###userName###のいいところはそのすべてです。ありのままの###userName###自身がいいところなのです。んなわけねぇだろ。',
+  '###userName###のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる###userName###が皆から評価されています。と同時に、衝動を解放したとき世界は終わります。'
 ];
 
 /**
